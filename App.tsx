c09eb3c023ca770a69e5f450ee4b50b8dff67708
Tony Li <dawner125@gmail.com>
2022-04-02 12:05:42 -0700

    Testing

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React = require('react');
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import { useState } from 'react';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
